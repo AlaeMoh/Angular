8bfd90dac990828d5cbc9fd0b9643c8e3426c823
AlaeMoh <alae.muhammad@gmail.com>
2025-07-19 16:41:19 +0300

    home

--- FlexBus/src/app/app-routing.module.ts
+++ FlexBus/src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { RegistrationComponent } from './registration/registration.component';
 import { FinaliseFlightComponent } from './finalise-flight/finalise-flight.component';
 
 const routes: Routes = [
+  {path:'',component:HomeComponent},
   {path:'home',component:HomeComponent},
   {path:'trains',component:TrainComponent},
   {path:'flights',component:FlightsComponent},
